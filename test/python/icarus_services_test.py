1f64bd69a4b124d42852431743a526c286cfb02e
Marco Del Tutto <mdeltutt@fnal.gov>
2025-02-27 10:39:55 -0600

    Add simple test for python services

---
+++ test/python/icarus_services_test.py
@@ -0,0 +1,10 @@
+
+import ICARUSservices
+
+geometry = ICARUSservices.ServiceManager('Geometry')
+wire_readout = ICARUSservices.ServiceManager('WireReadout')
+auxdet_geometry = ICARUSservices.ServiceManager('AuxDetGeometry')
+lar_properties = ICARUSservices.ServiceManager('LArProperties')
+det_clock = ICARUSservices.ServiceManager('DetectorClocks')
+det_properties = ICARUSservices.ServiceManager('DetectorProperties')
+
